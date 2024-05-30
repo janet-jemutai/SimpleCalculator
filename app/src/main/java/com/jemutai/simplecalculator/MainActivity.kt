b90d81f9d05b638625bd31bc55e3208b640799c4
Jemutai <janetjemutai19@gmail.com>
2024-05-30 09:00:54 +0300

    updated

--- app/src/main/java/com/jemutai/simplecalculator/MainActivity.kt
+++ app/src/main/java/com/jemutai/simplecalculator/MainActivity.kt
@@ -1,16 +1,34 @@
 package com.jemutai.simplecalculator
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.jemutai.simplecalculator.databinding.ActivityMainBinding
+import net.objecthunter.exp4j.Expression
 
 class MainActivity : AppCompatActivity() {
+
+    private  lateinit var  binding:ActivityMainBinding
+    var lastNumber = false
+    var stateError = false
+    var lastDot = false
+
+    private lateinit var  expression:Expression
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
     }
+
+    fun onClearClick(view: View) {}
+    fun onBackSpace(view: View) {}
+    fun onOperatorClick(view: View) {}
+    fun onDigitClick(view: View) {}
+    fun onClearAllClick(view: View) {}
 }
